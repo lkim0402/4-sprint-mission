7142f22f78035fedd8c9d0626f94eb35c984bc4e
Leejun <estherleejunkim@gmail.com>
2025-06-19 17:29:01 +0900

    각 도메인 모델 별 레포지토리 인터페이스 구현

--- src/main/java/com/sprint/mission/discodeit/repository/ReadStatusRepository.java
+++ src/main/java/com/sprint/mission/discodeit/repository/ReadStatusRepository.java
@@ -1,4 +1,17 @@
 package com.sprint.mission.discodeit.repository;
 
+import com.sprint.mission.discodeit.entity.ReadStatus;
+import com.sprint.mission.discodeit.entity.UserStatus;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
 public interface ReadStatusRepository {
+    ReadStatus save(ReadStatus readStatus);
+    Optional<ReadStatus> findById(UUID id);
+    Optional<ReadStatus> findByChannelAndUserId(UUID channelId, UUID userId);
+    List<ReadStatus> findAll();
+    boolean existsById(UUID id);
+    void deleteById(UUID id);
 }
--- src/main/java/com/sprint/mission/discodeit/repository/UserStatusRepository.java
+++ src/main/java/com/sprint/mission/discodeit/repository/UserStatusRepository.java
@@ -1,4 +1,16 @@
 package com.sprint.mission.discodeit.repository;
 
+import com.sprint.mission.discodeit.entity.BinaryContent;
+import com.sprint.mission.discodeit.entity.UserStatus;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
 public interface UserStatusRepository {
+    UserStatus save(UserStatus userStatus);
+    Optional<UserStatus> findByUserId(UUID userId);
+    List<UserStatus> findAll();
+    boolean existsById(UUID id);
+    void deleteById(UUID id);
 }
