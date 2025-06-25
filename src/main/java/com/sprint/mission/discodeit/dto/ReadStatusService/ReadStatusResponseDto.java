fab79a1dba0427029e7c3a76535ee6986928f112
Leejun <estherleejunkim@gmail.com>
2025-06-25 18:26:56 +0900

    readStatusServiceTest 테스팅 완료

--- src/main/java/com/sprint/mission/discodeit/dto/ReadStatusService/ReadStatusResponseDto.java
+++ src/main/java/com/sprint/mission/discodeit/dto/ReadStatusService/ReadStatusResponseDto.java
@@ -8,4 +8,15 @@ public record ReadStatusResponseDto(
     UUID userId,
     UUID channelId,
     Instant lastReadAt
-) {}
+) {
+    @Override
+    public String toString() {
+        return "\n" +
+                "    ReadStatusResponseDto {" + "\n" +
+                "    id         = " + this.id + ",\n" +
+                "    userId     = " + this.userId + ",\n" +
+                "    channelId  = " + this.channelId + ",\n" +
+                "    lastReadAt = " + this.lastReadAt + "\n" +
+                "  }";
+    }
+}
--- src/main/java/com/sprint/mission/discodeit/dto/ReadStatusService/ReadStatusResponseDtos.java
+++ src/main/java/com/sprint/mission/discodeit/dto/ReadStatusService/ReadStatusResponseDtos.java
@@ -1,11 +1,27 @@
 package com.sprint.mission.discodeit.dto.ReadStatusService;
 
+import com.sprint.mission.discodeit.dto.ChannelService.ChannelResponseDto;
 import com.sprint.mission.discodeit.dto.MessageService.MessageResponseDto;
+import com.sprint.mission.discodeit.entity.ReadStatus;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 public record ReadStatusResponseDtos(
         List<ReadStatusResponseDto> readStatusRequestDtos
 
 ) {
+    @Override
+    public String toString() {
+        if (readStatusRequestDtos == null || readStatusRequestDtos.isEmpty()) {
+            return "ReadStatus list: []";
+        }
+
+        // use a stream to call the toString() on each item in the list
+        // then join them together with a "," and a newline
+        return "\n--- List of ReadStatus ---" +
+                readStatusRequestDtos.stream()
+                        .map(ReadStatusResponseDto::toString) //
+                        .collect(Collectors.joining(","));
+    }
 }
