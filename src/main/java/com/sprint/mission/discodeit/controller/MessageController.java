352b305761a72ad604f096daffd4c5acd44fbc24
Leejun <estherleejunkim@gmail.com>
2025-07-02 18:16:56 +0900

    심화 요구사항 완성 + 코드 정리

--- src/main/java/com/sprint/mission/discodeit/controller/MessageController.java
+++ src/main/java/com/sprint/mission/discodeit/controller/MessageController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.UUID;
 
 @RestController
-@RequestMapping("/messages")
+@RequestMapping("/api/messages")
 @RequiredArgsConstructor
 
 public class MessageController {
