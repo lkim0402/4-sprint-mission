54d121ba50df953335ab5dfb1e7ae25237154fd9
Leejun <estherleejunkim@gmail.com>
2025-07-08 16:40:29 +0900

    GlobalExceptionHandler 추가

---
+++ src/main/java/com/sprint/mission/discodeit/dto/ErrorResponse.java
@@ -0,0 +1,18 @@
+package com.sprint.mission.discodeit.dto;
+
+import lombok.Getter;
+
+@Getter
+public class ErrorResponse {
+
+  private int status;
+  private String message;
+  private long timestamp;
+
+  public ErrorResponse(int status, String message) {
+    this.status = status;
+    this.message = message;
+    this.timestamp = System.currentTimeMillis();
+  }
+
+}
---
+++ src/main/java/com/sprint/mission/discodeit/exception/GlobalExceptionHandler.java
@@ -0,0 +1,42 @@
+package com.sprint.mission.discodeit.exception;
+
+import com.sprint.mission.discodeit.dto.ErrorResponse;
+import java.util.NoSuchElementException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+  @ExceptionHandler(NoSuchElementException.class)
+  public ResponseEntity<ErrorResponse> handleException(NoSuchElementException e) {
+    ErrorResponse errorResponse = new ErrorResponse(HttpStatus.NOT_FOUND.value(), e.getMessage());
+    return ResponseEntity
+        .status(HttpStatus.NOT_FOUND)
+        .body(errorResponse);
+  }
+
+  @ExceptionHandler(IllegalArgumentException.class)
+  public ResponseEntity<ErrorResponse> handleException(IllegalArgumentException e) {
+    ErrorResponse errorResponse = new ErrorResponse(HttpStatus.BAD_REQUEST.value(), e.getMessage());
+    return ResponseEntity
+        .status(HttpStatus.BAD_REQUEST)
+        .body(errorResponse);
+  }
+
+  @ExceptionHandler(Exception.class)
+  public ResponseEntity<String> handleException(Exception e) {
+    return ResponseEntity
+        .status(HttpStatus.INTERNAL_SERVER_ERROR)
+        .body(e.getMessage());
+  }
+
+  @ExceptionHandler(FileAccessException.class)
+  public ResponseEntity<String> handleFileAccessException(FileAccessException e) {
+    return ResponseEntity
+        .status(e.getStatus())
+        .body(e.getMessage());
+  }
+}
