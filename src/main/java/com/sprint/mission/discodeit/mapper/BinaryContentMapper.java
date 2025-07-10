3bdf4a516de9e07701ea33ef349377201c068aa5
Leejun <estherleejunkim@gmail.com>
2025-07-10 10:02:51 +0900

    코드 정리

--- src/main/java/com/sprint/mission/discodeit/mapper/BinaryContentMapper.java
+++ src/main/java/com/sprint/mission/discodeit/mapper/BinaryContentMapper.java
@@ -10,46 +10,6 @@ import java.util.List;
 @Component
 public class BinaryContentMapper {
 
-//  // Request
-//  public BinaryContent toBinaryContent(UUID userId, UUID messageId, MultipartFile file) {
-//
-//    if (file == null) {
-//      return null;
-//    }
-//    try {
-//      return new BinaryContent(
-//          userId,
-//          messageId,
-//          file.getBytes(),
-//          file.getOriginalFilename(),
-//          file.getContentType()
-//      );
-//    } catch (IOException e) {
-
-  /// /      throw new RuntimeException("Error reading file bytes"); /      throw new
-  /// IOException("Error reading file bytes");
-//      throw new FileAccessException("Error reading file bytes.", HttpStatus.NOT_ACCEPTABLE.value());
-//    }
-//  }
-//
-//  // Response
-//  public ResponseDto toBinaryContentResponseDto(BinaryContent binaryContent) {
-//    return new ResponseDto(
-//        binaryContent.getId(),
-//        binaryContent.getUserId(),
-//        binaryContent.getMessageId()
-//    );
-//  }
-//
-//  // Response
-//  public ResponseDtos toBinaryContentResponseDtos(List<BinaryContent> binaryContents) {
-//    return new ResponseDtos(
-//        binaryContents
-//            .stream()
-//            .map(this::toBinaryContentResponseDto)
-//            .toList()
-//    );
-//  }
   public BinaryContentResponse toResponseDto(BinaryContent binaryContent) {
     return new BinaryContentResponse(
         binaryContent.getId(),
