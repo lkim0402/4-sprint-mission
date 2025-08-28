12d162815f54b6aca9ec0196714ebc8ab46c2266
Leejun <estherleejunkim@gmail.com>
2025-08-28 09:30:55 +0900

    추가 application-test.yml

--- src/main/java/com/sprint/mission/discodeit/DiscodeitApplication.java
+++ src/main/java/com/sprint/mission/discodeit/DiscodeitApplication.java
@@ -2,8 +2,10 @@ package com.sprint.mission.discodeit;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootApplication
+@ActiveProfiles("test")
 public class DiscodeitApplication {
 
   public static void main(String[] args) {
