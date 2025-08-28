4c1203db0111e8f4ee1f11c83120c2f87688f4a6
Leejun <estherleejunkim@gmail.com>
2025-08-28 09:38:23 +0900

    추가 ActiveProfiles

--- src/test/java/com/sprint/mission/discodeit/DiscodeitApplicationTests.java
+++ src/test/java/com/sprint/mission/discodeit/DiscodeitApplicationTests.java
@@ -3,7 +3,9 @@ package com.sprint.mission.discodeit;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.ActiveProfiles;
 
+@ActiveProfiles("test")
 @SpringBootTest
 class DiscodeitApplicationTests {
 
